4bb11ccf01f5a54b45f8f4ddd117e7ed52120055
Luz0801 <118777973+Luz0801@users.noreply.github.com>
2023-10-14 11:11:51 -0500

    update final

--- src/index.js
+++ src/index.js
@@ -1,171 +1,65 @@
-import './css/styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
-import { fetchCountries } from './js/fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const elements = {
+  selectEl: document.querySelector('.breed-select'),
+  textMarkEl: document.querySelector('.cat-info'),
+  loaderEl: document.querySelector('.loader'),
+  errorEl: document.querySelector('.error'),
+};
 
-const countriesList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-const searchBox = document.querySelector('#search-box');
-const body = document.querySelector('body');
+const { selectEl, textMarkEl, loaderEl, errorEl } = elements;
 
-body.style.backgroundImage =
-  'radial-gradient( circle 610px at 5.2% 51.6%,  rgba(5,8,114,1) 0%, rgba(7,3,53,1) 97.5% )';
-countriesList.style.visibility = 'hidden';
-countryInfo.style.visibility = 'hidden';
+// loaderEl.classList.replace('loader', 'is-hidden');
+// errorEl.classList.add('is-hidden');
+textMarkEl.classList.add('is-hidden');
 
-searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+selectEl.addEventListener('change', createMarkUp);
 
-// ---------- 1st variant --------------------
-// function onInputSearch(e) {
-//     e.preventDefault();
+updateSelect();
 
-//     const searchCountries = e.target.value.trim();
-
-//     if (!searchCountries) {
-//         countriesList.style.visibility = "hidden";
-//         countryInfo.style.visibility = "hidden";
-//         countriesList.innerHTML = '';
-//         countryInfo.innerHTML = '';
-//         return;
-//     }
-
-//     fetchCountries(searchCountries)
-//         .then(result => {
-//             if (result.length > 10) {
-//                 Notify.info('Too many matches found. Please, enter a more specific name.');
-//                 return;
-//             }
-//             renderedCountries(result);
-//         })
-//         .catch(error => {
-//             countriesList.innerHTML = '';
-//             countryInfo.innerHTML = '';
-//             Notify.failure('Oops, there is no country with that name');
-//         })
-// };
-
-// function renderedCountries(result) {
-//     const inputLetters = result.length;
-
-//     if (inputLetters === 1) {
-//         countriesList.innerHTML = '';
-//         countriesList.style.visibility = "hidden";
-//         countryInfo.style.visibility = "visible";
-//         countryCardMarkup(result);
-//     }
-
-//     if (inputLetters > 1 && inputLetters <= 10) {
-//         countryInfo.innerHTML = '';
-//         countryInfo.style.visibility = "hidden";
-//         countriesList.style.visibility = "visible";
-//         countriesListMarkup(result);
-//     }
-// }
-
-// function countriesListMarkup(result) {
-//     const listMarkup = result.map((({ name, flags }) => {
-//         return /*html*/ `<li>
-//                         <img src="${flags.svg}" alt="${name}" width="60" height="auto">
-//                         <span>${name.official}</span>
-//                 </li>`;
-//     })).join('');
-//     countriesList.innerHTML = listMarkup;
-//     return listMarkup;
-// }
-
-// function countryCardMarkup(result) {
-//     const cardMarkup = result.map(({ flags, name, capital, population, languages }) => {
-//         languages = Object.values(languages).join(", ");
-//         return /*html*/ `
-//             <img src="${flags.svg}" alt="${name}" width="320" height="auto">
-//             <p> ${name.official}</p>
-//             <p>Capital: <span> ${capital}</span></p>
-//             <p>Population: <span> ${population}</span></p>
-//             <p>Languages: <span> ${languages}</span></p>`;
-//     }).join('');
-//     countryInfo.innerHTML = cardMarkup;
-//     return cardMarkup;
-// }
-
-//  ---------- 2nd variant -------------------------
-
-function onInputSearch(e) {
-  const value = searchBox.value.trim();
-  console.log(value);
-
-  if (!value) {
-    addHidden();
-    clearInterfaceUI();
-    return;
-  }
-
-  fetchCountries(value)
+function updateSelect(data) {
+  fetchBreeds(data)
     .then(data => {
-      if (data.length > 10) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      }
-      renderCountries(data);
+      loaderEl.classList.replace('loader', 'is-hidden');
+
+      let markSelect = data.map(({ name, id }) => {
+        return `<option value ='${id}'>${name}</option>`;
+      });
+      selectEl.insertAdjacentHTML('beforeend', markSelect);
+      new SlimSelect({
+        select: selectEl,
+      });
     })
-    .catch(err => {
-      clearInterfaceUI();
-      Notify.failure('Oops, there is no country with that name');
-    });
+    .catch(onFetchError);
 }
 
-const generateMarkupCountryInfo = data =>
-  data.reduce(
-    (acc, { flags: { svg }, name, capital, population, languages }) => {
-      console.log(languages);
-      languages = Object.values(languages).join(', ');
-      console.log(name);
-      return (
-        acc +
-        ` <img src="${svg}" alt="${name}" width="320" height="auto">
-            <p> ${name.official}</p>
-            <p>Capital: <span> ${capital}</span></p>
-            <p>Population: <span> ${population}</span></p>
-            <p>Languages: <span> ${languages}</span></p>`
-      );
-    },
-    ''
+function createMarkUp(event) {
+  loaderEl.classList.replace('is-hidden', 'loader');
+  selectEl.classList.add('is-hidden');
+  textMarkEl.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loaderEl.classList.replace('loader', 'is-hidden');
+      selectEl.classList.remove('is-hidden');
+      const { url, breeds } = data[0];
+
+      textMarkEl.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
+      textMarkEl.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError() {
+  selectEl.classList.remove('is-hidden');
+  loaderEl.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
   );
-
-const generateMarkupCountryList = data =>
-  data.reduce((acc, { name: { official, common }, flags: { svg } }) => {
-    return (
-      acc +
-      `<li>
-        <img src="${svg}" alt="${common}" width="70">
-        <span>${official}</span>
-      </li>`
-    );
-  }, '');
-
-function renderCountries(result) {
-  if (result.length === 1) {
-    countriesList.innerHTML = '';
-    countriesList.style.visibility = 'hidden';
-    countryInfo.style.visibility = 'visible';
-    countryInfo.innerHTML = generateMarkupCountryInfo(result);
-  }
-  if (result.length >= 2 && result.length <= 10) {
-    countryInfo.innerHTML = '';
-    countryInfo.style.visibility = 'hidden';
-    countriesList.style.visibility = 'visible';
-    countriesList.innerHTML = generateMarkupCountryList(result);
-  }
-}
-
-function clearInterfaceUI() {
-  countriesList.innerHTML = '';
-  countryInfo.innerHTML = '';
-}
-
-function addHidden() {
-  countriesList.style.visibility = 'hidden';
-  countryInfo.style.visibility = 'hidden';
 }
